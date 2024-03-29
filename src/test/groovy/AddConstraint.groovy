abb89dfeaa10a9f1a920bdadb5a35cdd40e6d766
yYou <yvonnickyou@lttd-consulting.com>
2024-03-29 10:38:53 +0100

    Livraison

--- src/test/groovy/AddConstraint.groovy
+++ src/test/groovy/AddConstraint.groovy
@@ -25,6 +25,7 @@ public class AddConstraint extends ExtendM3Transaction {
   private String ZGKY
   private Integer zblc
   private Integer ztps
+  private Integer ztp1
 
   public AddConstraint(MIAPI mi, DatabaseAPI database, ProgramAPI program, MICallerAPI miCaller, UtilityAPI utility) {
     this.mi = mi;
